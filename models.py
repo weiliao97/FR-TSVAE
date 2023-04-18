a679ff14588eebaa473090c39dd644a8865bd8b8
root <liaoweithu@gmail.com>
2023-04-18 15:41:39 +0000

    no time series

--- models.py
+++ models.py
@@ -177,7 +177,7 @@ class TemporalConvDec(nn.Module):
         num_levels = len(num_channels)
         for i in range(num_levels):
             dilation_size = 2 ** i
-            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            in_channels = 2 if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
                                      padding=(kernel_size-1) * dilation_size, dropout=dropout)]
